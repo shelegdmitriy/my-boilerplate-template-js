07b19c21dbc505748a0eb6aef618b3d402b46db9
Dmitriy <34593263+shelegdmitriy@users.noreply.github.com>
2023-01-24 20:59:15 +0200

    Initial commit

---
+++ contract/deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+./build.sh
+
+if [ $? -ne 0 ]; then
+  echo ">> Error building contract"
+  exit 1
+fi
+
+echo ">> Deploying contract"
+
+# https://docs.near.org/tools/near-cli#near-dev-deploy
+near dev-deploy --wasmFile build/hello_near.wasm
+
+CONTRACT_ADDRESS=`cat ./neardev/dev-account`
+
+cd ../frontend
+
+touch .env.local
+
+echo NEXT_PUBLIC_CONTRACT_NAME=$CONTRACT_ADDRESS >> .env.local
