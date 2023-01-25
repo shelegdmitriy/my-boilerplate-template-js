b6ca2a93aa0189f7763c32b2d2ab18862070f89f
Dmitriy <sheleg.dmitriy@gmail.com>
2023-01-25 15:25:34 +0200

    Add assetPrefix

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+}
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: assetPrefix
 };
 
 module.exports = nextConfig;
